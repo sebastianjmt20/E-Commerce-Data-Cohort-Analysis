79572370e7ecdde6f646ca65d924d74a472f74d0
Sebastian Millan <sebastianjmt20@gmail.com>
2026-08-26 22:39:02 -0400

    hot fix

--- 04_customer_retention.sql
+++ 04_customer_retention.sql
@@ -3,22 +3,37 @@ WITH RAW_VALUES AS(
     CASE 
         WHEN max_date > (SELECT (max(max_date) - INTERVAL'6 MONTH')::date FROM dayly_revenue)  THEN  'ACTIVE'
         ELSE 'CHURNED'
-    END as status
+    END as status,
+    total_revenue
     FROM dayly_revenue
     WHERE min_date < (SELECT (max(max_date) - INTERVAL'6 MONTH')::date FROM dayly_revenue)
 )
 
-SELECT extract(YEAR FROM max_date) as year,
-CASE WHEN max_date > (SELECT (max(max_date) - INTERVAL'6 MONTH')::date FROM dayly_revenue GROUP BY extract(YEAR FROM max_date))  THEN  count(*) END AS ACTIVE,
-CASE WHEN max_date < (SELECT (max(max_date) - INTERVAL'6 MONTH')::date FROM dayly_revenue GROUP BY extract(YEAR FROM max_date))  THEN  count(*) END AS CHURNED
-FROM dayly_revenue
-WHERE min_date < (SELECT (max(max_date) - INTERVAL'6 MONTH' GROUP BY extract(YEAR FROM max_date))::date FROM dayly_revenue)
-GROUP BY year
+SELECT status, count(*),sum(count(*)) OVER() as total, round((count(*)/sum(count(*)) OVER() *100)::NUMERIC,2) as percent, ROUND(SUM(total_revenue)::NUMERIC,2) AS revenue
+FROM RAW_VALUES
+GROUP BY status
 
+
+--SELECT (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE FROM dayly_revenue
+--GROUP BY (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month');
+
+SELECT extract(YEAR FROM min_date) as year,
+count(CASE WHEN max_date > (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE and min_date < (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE THEN 'ACTIVE' END) AS ACTIVE,
+count(CASE WHEN max_date <= (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE or min_date > (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE THEN  'CHURNED'END) AS CHURNED,
+count(*) year_total,
+ROUND((count(CASE WHEN max_date > (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE and min_date < (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE THEN 'ACTIVE' END)::INTEGER 
+/SUM(count(*)) OVER(PARTITION BY extract(YEAR FROM min_date)) * 100)::NUMERIC,2) AS percert_active,
+ROUND(((count(CASE WHEN max_date < (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE or min_date > (('31-12-' || extract(YEAR FROM max_date))::date - INTERVAL '6 month')::DATE THEN  'CHURNED'END)::INTEGER 
+/SUM(count(*)) OVER(PARTITION BY extract(YEAR FROM min_date))) * 100)::NUMERIC,2) AS percert_churn,
+ROUND(SUM(total_revenue)::NUMERIC,2) AS revenue
+FROM dayly_revenue
+WHERE extract(YEAR FROM min_date) < 2024
+GROUP BY year 
+ORDER BY YEAR
+
+SELECT count(*) from dayly_revenue
+WHERE extract(YEAR from min_date) = 2015
 -- SELECT * from RAW_VALUES
--- SELECT status, count(*)--, (count(*)/(SELECT count(*) from RAW_VALUES)*100) 
--- FROM RAW_VALUES
--- GROUP BY status
 
 
 
